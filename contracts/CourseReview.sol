5caec54ef83c72e7931a9b40863b49bb87e17bb9
anshgupta25 <gupta2020ansh@gmail.com>
2022-11-29 23:10:56 +0530

    initail commit

--- contracts/CourseReview.sol
+++ contracts/CourseReview.sol
@@ -1,16 +1,19 @@
-pragma solidity >=0.4.21 <0.7.0;
+// SPDX-License-Identifier: MIT License
+
+pragma solidity >=0.4.0 <0.7.0;
 
 contract CourseReview{
     // model a condidate 
     struct Course{
       uint id;
       string name;
-      uint voteCount;
-      uint rating;
+      string desc;
+      uint num_donation;
+      uint amt_collect;
       //make a map of the students who have voted for the course
 
       mapping(address => bool) students;
-      mapping(address => uint) ratings;
+      mapping(address => uint) amt;
     }
     // store accounts that have voted
     
@@ -18,65 +21,58 @@ contract CourseReview{
    // fetch course 
     mapping(uint => Course) public courses;
     // store course count
-   uint public coursesCount;
+   uint public charity_num;
 
     constructor() public{
-      addCourse("BlockChain Technology");
-      addCourse("Machine Learning");
+      addCourse("Charity 1","Default Charity");
     }
 
-    function addCourse (string memory _name) public {
-      coursesCount++;
+    function addCourse (string memory _name, string memory _desc) public {
+      charity_num++;
       //add a new course
-      courses[coursesCount] = Course(coursesCount, _name, 0, 0);
-      // courses[coursesCount] = Course(coursesCount,_name,0);
+      courses[charity_num] = Course(charity_num, _name,_desc, 0, 0);
+      // courses[charity_num] = Course(charity_num,_name
     }
     
     function vote (uint _courseId,uint _rating) public {
       // address has not voted before
       // require(!students[msg.sender]);
       // voting for valid course
-      require(_courseId>0 && _courseId<=coursesCount);
+      require(_courseId>0 && _courseId<=charity_num);
       // update the voter has voted
-      //rating must be between 1 and 5
-      require(_rating>0 && _rating<=5);
+      //amt_collect must be between 1 and 5
+      require(_rating>0);
 
       // students[msg.sender] = true;
       //update the votes of course
-      //update the rating of the course
+      //update the amt_collect of the course
       //make a variable tempcourse
       Course memory tempCourse = courses[_courseId];
       // var tempCourse = courses[_courseId];
-      uint tempCount = tempCourse.voteCount;
-      uint tempRating = tempCourse.rating;
-      tempRating = tempRating*tempCount ;
+      uint tempCount = tempCourse.num_donation;
+      uint tempRating = tempCourse.amt_collect;
       // tempRating = tempRating + _rating;
 
       //if the student has voted before then the vote count should not be incremented
 
-      if(!courses[_courseId].students[msg.sender]){
         tempCount++;
         courses[_courseId].students[msg.sender] = true;
-        courses[_courseId].ratings[msg.sender] = _rating;
         tempRating = tempRating + _rating;
-      }
-      else{
-        tempRating = tempRating - courses[_courseId].ratings[msg.sender] + _rating;
-        courses[_courseId].ratings[msg.sender] = _rating;
-      }
-      //make a for loop to calculate the average rating of specific course
+        courses[_courseId].amt[msg.sender] = tempRating;
+        
+    
+      //make a for loop to calculate the average amt_collect of specific course
       // uint sum = 0;
       // uint count = 0;
       // for(uint i=1;i<=tempCount;i++){
-      //   //make a for loop on ratings
-      //   sum = sum + courses[_courseId].ratings[i];
+      //   //make a for loop on amt
+      //   sum = sum + courses[_courseId].amt[i];
       // }
     
 
       // tempCount++;
-      tempRating = tempRating/tempCount;
-      courses[_courseId] = Course(_courseId,tempCourse.name,tempCount,tempRating);
-      // courses[_courseId].voteCount ++;
+      courses[_courseId] = Course(_courseId,tempCourse.name,tempCourse.desc,tempCount,tempRating);
+      // courses[_courseId].num_donation ++;
     }
 
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,156 +1,155 @@
 App = {
-  web3Provider: null,
-  contracts: {},
-  account: '0x0',
+    web3Provider: null,
+    contracts: {},
+    account: '0x0',
 
-  init: function() {
-    return App.initWeb3();
-  },
+    init: function() {
+        return App.initWeb3();
+    },
 
-  initWeb3: function() {
-    // TODO: refactor conditional
-    if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
-    } else {
-      // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-      web3 = new Web3(App.web3Provider);
-    }
-    return App.initContract();
-  },
+    initWeb3: function() {
+        // TODO: refactor conditional
+        if (typeof web3 !== 'undefined') {
+            // If a web3 instance is already provided by Meta Mask.
+            App.web3Provider = web3.currentProvider;
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            // Specify default instance if no web3 instance provided
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            web3 = new Web3(App.web3Provider);
+        }
+        return App.initContract();
+    },
 
-  initContract: function() {
-    $.getJSON("CourseReview.json", function(courseReview) {
-      // Instantiate a new truffle contract from the artifact
-      App.contracts.CourseReview = TruffleContract(courseReview);
-      // Connect provider to interact with contract
-      App.contracts.CourseReview.setProvider(App.web3Provider);
+    initContract: function() {
+        $.getJSON("CourseReview.json", function(courseReview) {
+            // Instantiate a new truffle contract from the artifact
+            App.contracts.CourseReview = TruffleContract(courseReview);
+            // Connect provider to interact with contract
+            App.contracts.CourseReview.setProvider(App.web3Provider);
 
-      App.listenForEvents();
+            App.listenForEvents();
 
-      return App.render();
-    });
-  },
-
-  // Listen for events emitted from the contract
-  listenForEvents: function() {
-    App.contracts.CourseReview.deployed().then(function(instance) {
-      // Restart Chrome if you are unable to receive this event
-      // This is a known issue with Metamask
-      // https://github.com/MetaMask/metamask-extension/issues/2393
-      instance.votedEvent({}, {
-        fromBlock: 0,
-        toBlock: 'latest'
-      }).watch(function(error, event) {
-        console.log("event triggered", event)
-        console.log("yo fucker")
-        // Reload when a new vote is recorded
-        App.render();
-      });
-    });
-  },
-
-  render: function() {
-    var courseReviewInstance;
-    var loader = $("#loader");
-    var content = $("#content");
-
-    loader.show();
-    content.hide();
-
-    // Load account data
-    web3.eth.getCoinbase(function(err, account) {
-      if (err === null) {
-        App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
-      }
-    });
-
-    // Load contract data
-    App.contracts.CourseReview.deployed().then(function(instance) {
-      courseReviewInstance = instance;
-      return courseReviewInstance.coursesCount();
-    }).then(function(coursesCount) {
-      var coursesResults = $("#coursesResults");
-      coursesResults.empty();
-
-      var coursesSelect = $('#coursesSelect');
-      coursesSelect.empty();
-
-      for (var i = 1; i <= coursesCount; i++) {
-        courseReviewInstance.courses(i).then(function(course) {
-          var id = course[0];
-          var name = course[1];
-          var voteCount = course[2];
-          var rating = course[3];
-          console.log("id: " + id + " name: " + name + " voteCount: " + voteCount + " rating: " + rating);
-
-          // Render course Result
-          var courseTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td><td>" + rating + "</tr>"
-          coursesResults.append(courseTemplate);
-
-          // Render course ballot option
-          var courseOption = "<option value='" + id + "' >" + name + "</ option>"
-          coursesSelect.append(courseOption);
+            return App.render();
         });
-      }
-      // return courseReviewInstance.students(App.account);
-      
-    }).then(function() {
-      // Do not allow a user to vote
-      // if() {
-      //   $('form').hide();
-      //   console.log(": " );
-      // }
-      loader.hide();
-      content.show();
-    }).catch(function(error) {
-      console.warn(error);
-      console.log("error: " + error);
-    });
-  },
+    },
 
-  castVote: function() {
-    var courseId = $('#coursesSelect').val();
-    var courseRating = $('#courseRating').val();
-    console.log("courseId: " + courseId + " courseRating: " + courseRating);
-    App.contracts.CourseReview.deployed().then(function(instance) {
-      //return courseId and courseRating to the contract
-      return instance.vote(courseId, courseRating, { from: App.account });
-      // return instance.vote(courseId,courseRating, { from: App.account });
-    }).then(function(result) {
-      // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
-    }).catch(function(err) {
-      console.error(err);
-    });
-  },
-  addCourse: function() {
-    var courseName = $('#courseName').val();
-    console.log("courseName: " + courseName);
-    App.contracts.CourseReview.deployed().then(function(instance) {
-      //return courseId and courseRating to the contract
-      return instance.addCourse(courseName, { from: App.account });
-      // return instance.vote(courseId,courseRating, { from: App.account });
-    }).then(function(result) {
-      // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
-    }).catch(function(err) {
-      console.error(err);
-    });
+    // Listen for events emitted from the contract
+    listenForEvents: function() {
+        App.contracts.CourseReview.deployed().then(function(instance) {
+            // Restart Chrome if you are unable to receive this event
+            // This is a known issue with Metamask
+            // https://github.com/MetaMask/metamask-extension/issues/2393
+            instance.votedEvent({}, {
+                fromBlock: 0,
+                toBlock: 'latest'
+            }).watch(function(error, event) {
+                // Reload when a new vote is recorded
+                App.render();
+            });
+        });
+    },
 
-  }
+    render: function() {
+        var courseReviewInstance;
+        var loader = $("#loader");
+        var content = $("#content");
+
+        loader.show();
+        content.hide();
+
+        // Load account data
+        web3.eth.getCoinbase(function(err, account) {
+            if (err === null) {
+                App.account = account;
+                $("#accountAddress").html("Your Account: " + account);
+            }
+        });
+
+        // Load contract data
+        App.contracts.CourseReview.deployed().then(function(instance) {
+            courseReviewInstance = instance;
+            return courseReviewInstance.charity_num(); //////////////////////////////////////////////////////
+        }).then(function(charity_num) {
+            var CHARITY = $("#CHARITY");
+            CHARITY.empty();
+
+            var SELECT_CHARITY = $('#SELECT_CHARITY');
+            SELECT_CHARITY.empty();
+
+            for (var i = 1; i <= charity_num; i++) {
+                courseReviewInstance.courses(i).then(function(course) {
+                    var id = course[0];
+                    var name = course[1];
+                    var desc = course[2];
+                    var voteCount = course[3];
+                    var rating = course[4];
+
+                    // Render course Result
+                    var courseTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + desc + "</td><td>" + voteCount + "</td><td>" + rating + "</tr>"
+                    CHARITY.append(courseTemplate);
+
+                    // Render course ballot option
+                    var courseOption = "<option value='" + id + "' >" + name + "</ option>"
+                    SELECT_CHARITY.append(courseOption);
+                });
+            }
+            // return courseReviewInstance.students(App.account);
+
+        }).then(function() {
+            // Do not allow a user to vote
+            // if() {
+            //   $('form').hide();
+            //   console.log(": " );
+            // }
+            loader.hide();
+            content.show();
+        }).catch(function(error) {
+            console.warn(error);
+            console.log("error: " + error);
+        });
+    },
+
+    castVote: function() {
+        var courseId = $('#SELECT_CHARITY').val();
+        var courseRating = $('#courseRating').val();
+        console.log("courseId: " + courseId + " courseRating: " + courseRating);
+        App.contracts.CourseReview.deployed().then(function(instance) {
+            //return courseId and courseRating to the contract
+            return instance.vote(courseId, courseRating, { from: App.account });
+            // return instance.vote(courseId,courseRating, { from: App.account });
+        }).then(function(result) {
+            // Wait for votes to update
+            $("#content").hide();
+            $("#loader").show();
+        }).catch(function(err) {
+            console.error(err);
+        });
+    },
+    addCourse: function() {
+        var courseName = $('#courseName').val();
+        var courseDesc = $('#courseDesc').val();
+        console.log("courseName: " + courseName);
+        App.contracts.CourseReview.deployed().then(function(instance) {
+            //return courseId and courseRating to the contract
+            return instance.addCourse(courseName, courseDesc, { from: App.account });
+            // return instance.vote(courseId,courseRating, { from: App.account });
+        }).then(function(result) {
+            // Wait for votes to update
+            $("#content").hide();
+            $("#loader").show();
+        }).catch(function(err) {
+            console.error(err);
+        });
+
+    }
 
 
 
 };
 
 $(function() {
-  $(window).load(function() {
-    App.init();
-  });
+    $(window).load(function() {
+        App.init();
+    });
 });
