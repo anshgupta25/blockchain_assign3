e0fca1efc0003897bc4143a5f16512222e9912a9
Abhiinv <abhiinv24@gmail.com>
2022-12-01 02:21:55 +0530

    Update index.html

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ module.exports = {
     // for more about customizing your Truffle configuration!
     networks: {
         development: {
-            host: "172.16.127.65",
+            host: "127.0.0.1",
             port: 7545,
             network_id: 5777 // Match any network id
         },
